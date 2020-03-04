953c78b63fe911a1b30ff4313ec575225ffef1bf
Tim Lee <timlee@Sols-MacBook-Air.local>
2020-03-04 10:28:37 -0500

    fixes and new scraping function

--- io_testing.py
+++ io_testing.py
@@ -1,4 +1,5 @@
 from movies_io import *
+import time
 '''
 # test getting single IMDb ID from a TMDb ID
 tmdb_id = 475303
@@ -17,8 +18,9 @@ imdb_id = 'tt1502397'
 imdb_info = get_imdb_info(imdb_id)
 print(imdb_info)
 '''
+'''
 # test end-to-end, pass in single TMDb ID and return two lists: the box office info, and the IMDb info
-tmdb_id = 466282
+tmdb_id = 8835
 imdb_id = get_imdb_ID(tmdb_id)
 box_office = get_box_office(imdb_id)
 imdb_info = get_imdb_info(imdb_id)
@@ -26,3 +28,19 @@ imdb_info = get_imdb_info(imdb_id)
 print(imdb_id)
 print(box_office)
 print(imdb_info)
+'''
+
+# test end-to-end, pass in multiple TMDb IDs and return multiple sets of two lists: the box office info, and the IMDb info
+
+tmdb_ids = [458897, 8835, 512200, 399174]
+for id in tmdb_ids:
+    imdb_id = get_imdb_ID(id)
+    movie_details = get_movie_details(id)
+    box_office = get_box_office(imdb_id)
+    imdb_info = get_imdb_info(imdb_id)
+
+    print(imdb_id)
+    print(movie_details)
+    print(box_office)
+    print(imdb_info)
+    time.sleep(1)
